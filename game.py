4a1842b5d038204b7967e876efbf7dd8fa7ee5f6
FIEF-Nohell <noel.hmnn@gmail.com>
2023-08-11 16:26:17 +0200

    requirements and main file

---
+++ game.py
@@ -0,0 +1,14 @@
+import pygame
+import sys
+
+# Initialize pygame
+pygame.init()
+
+# Constants
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+SAND_SIZE = 4
+
+# Colors
+WHITE = (255, 255, 255)
+YELLOW = (255, 255, 0)
