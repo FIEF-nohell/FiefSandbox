1c62f811bd410e89d58a016a00614ae279afde6f
FIEF-Nohell <noel.hmnn@gmail.com>
2023-08-11 16:36:01 +0200

    hold rmb to drop more sand

--- game.py
+++ game.py
@@ -51,15 +51,29 @@ def draw_sand():
 
 def main():
     clock = pygame.time.Clock()
+    placing_sand = False
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
-            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:  # Left click
+            if event.type == pygame.MOUSEBUTTONDOWN:
                 x, y = pygame.mouse.get_pos()
                 x = (x // SAND_SIZE) * SAND_SIZE  # Make the sand placement grid-aligned
                 y = (y // SAND_SIZE) * SAND_SIZE
+                if event.button == 1:  # Left click
+                    sand_positions.append((x, y))
+                elif event.button == 3:  # Right click
+                    placing_sand = True
+            if event.type == pygame.MOUSEBUTTONUP and event.button == 3:  # Release right click
+                placing_sand = False
+
+        if placing_sand:
+            x, y = pygame.mouse.get_pos()
+            x = (x // SAND_SIZE) * SAND_SIZE
+            y = (y // SAND_SIZE) * SAND_SIZE
+            if (x, y) not in sand_positions:
                 sand_positions.append((x, y))
 
         drop_sand()
@@ -69,6 +83,7 @@ def main():
 
         pygame.display.flip()
         clock.tick(60)
+
 
 if __name__ == "__main__":
     main()
